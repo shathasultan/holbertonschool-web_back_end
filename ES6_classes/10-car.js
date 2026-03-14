51ee0b3bbb1aacb1bae9a181ff29183dd4aa3223
SHATHA SULTAN AL-SWAILAM <154978695+shathasultan@users.noreply.github.com>
2026-03-14 22:21:33 +0300

    Add Car class with constructor and clone method

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,26 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    // if (typeof brand !== 'string') {
+    //   throw TypeError('Brand must be a string');
+    // }
+    // if (typeof motor !== 'string') {
+    //   throw TypeError('Motor must be a string');
+    // }
+    // if (typeof color !== 'string') {
+    //   throw TypeError('Color must be a string');
+    // }
+
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    // return new Car(
+    //   String(this._brand),
+    //   String(this._motor),
+    //   String(this._color),
+    // );
+    return new this.constructor[Symbol.species]();
+  }
+}
