f655fda2d3b487490a649b4a6553c6220b5eac38
SHATHA SULTAN AL-SWAILAM <154978695+shathasultan@users.noreply.github.com>
2026-03-14 22:31:31 +0300

    Update 10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,26 +1,16 @@
 export default class Car {
   constructor(brand, motor, color) {
-    // if (typeof brand !== 'string') {
-    //   throw TypeError('Brand must be a string');
-    // }
-    // if (typeof motor !== 'string') {
-    //   throw TypeError('Motor must be a string');
-    // }
-    // if (typeof color !== 'string') {
-    //   throw TypeError('Color must be a string');
-    // }
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
-    // return new Car(
-    //   String(this._brand),
-    //   String(this._motor),
-    //   String(this._color),
-    // );
-    return new this.constructor[Symbol.species]();
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
