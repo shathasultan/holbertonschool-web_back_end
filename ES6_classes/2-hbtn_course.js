4bae2fa488b52931333655c1b3d42bd60b9280cd
SHATHA SULTAN AL-SWAILAM <154978695+shathasultan@users.noreply.github.com>
2026-03-14 22:23:18 +0300

    Create HolbertonCourse class with validation
    Implemented HolbertonCourse class with validation for name, length, and students.

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,62 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (
+      !Array.isArray(students)
+      || !students.every((student) => typeof student === 'string')
+    ) {
+      throw TypeError('Students must be an array of strings');
+    }
+    if (length <= 0) {
+      throw RangeError('Length must be greater than 0');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (newLength <= 0) {
+      throw RangeError('Length must be greater than 0');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (
+      !Array.isArray(newStudents)
+      || !newStudents.every((student) => typeof student === 'string')
+    ) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
